d2f1e0ae93ff26e184ac710b4825396d649f9e5e
Rashed Al Ghailani <Authority217@gmail.com>
2024-12-12 13:43:23 +0400

    Setting Up the Database Context

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DTOsTask.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DTOsTask
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
